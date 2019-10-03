b63b4cbbedfd647b5b8f66ed32799821fc240036
sasha <testreact2@yandex.ru>
2019-10-03 19:22:32 +0300

    create package.json

--- config/webpack.js
+++ config/webpack.js
@@ -1,12 +1,20 @@
 const path = require("path");
 const Dotenv = require('dotenv-webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CreateFileWebpack = require('create-file-webpack');
+const package_orig_json = require("../package.json");
+const package_new_json = JSON.parse(JSON.stringify(package_orig_json));
+delete package_new_json.devDependencies;
+delete package_new_json.scripts;
+
+const dist_path =  path.join(__dirname, "../dist");
+
 require('dotenv').config();
 module.exports = {
     devtool: 'source-map',
     entry: "./src/index.js",
     output: {
-        path: path.join(__dirname, "../dist"),
+        path: dist_path,
         filename: "index.js",
         library: "models",
         libraryTarget: 'umd',
@@ -36,5 +44,6 @@ module.exports = {
     plugins: [
 	new Dotenv(),
         new  CleanWebpackPlugin(),
+        new CreateFileWebpack({path: dist_path, fileName: "package.json", content: JSON.stringify(package_new_json, null, 2)})
     ]
 }
